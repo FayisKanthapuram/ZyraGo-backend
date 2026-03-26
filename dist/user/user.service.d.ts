766ccff2247f533731f18029fe8a6871f87e0944
FayisKanthapuram <fayiskanthapuram@gmail.com>
2026-03-26 11:02:23 +0530

    chore: update .gitignore and remove node_modules/dist from index

--- dist/user/user.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Model } from 'mongoose';
-import { UserDocument } from './schemas/user.schema';
-export declare class UserService {
-    private userModel;
-    constructor(userModel: Model<UserDocument>);
-    create(userData: any): Promise<UserDocument>;
-    findOneByEmail(email: string): Promise<UserDocument | null>;
-}
